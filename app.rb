d802eb225b3532c12b43efb385a0d384c21f00c1
Jason Tham <jtham@sbcglobal.net>
2015-11-24 11:01:47 -0600

    init

---
+++ app.rb
@@ -0,0 +1,38 @@
+require "bundler"
+Bundler.require
+
+# get '/' do
+#   return {:hello => 'hello, world'}.to_json
+# end
+
+get '/' do
+  erb :hello
+end
+
+get '/worlds' do
+  erb :world
+end
+
+get '/submit' do
+  erb :submit
+end
+
+post '/submit_form' do
+  p params
+  return params.to_json
+end
+
+get '/main' do
+  erb :main
+end
+
+get '/stuff' do
+  erb :stuff
+end
+
+get '/happy' do
+  happy_things = Dictionary.new({
+    :theworld => 'is awesome'
+    })
+    return happy_things.to_json
+end
---
+++ config.ru
@@ -0,0 +1,3 @@
+require "./app"
+require "./dictionary"
+run Sinatra::Application
